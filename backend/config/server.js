b30ec03bc4f1306543fd5a300ce090911e3c60cc
Rhys Morris <rhysmorris08@gmail.com>
2021-09-19 14:12:18 +1000

    Seperate client and backend and remove parent directory

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '1a42133aea560755ad4ef10cf76f23da'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '396e8415-8f0b-4ad3-9871-4ec1d087cfb5'
-};
